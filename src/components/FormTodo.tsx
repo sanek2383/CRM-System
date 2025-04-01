b49ba3ffd5f74ae83159a3b53d3bcf2dcf8e1414
Alexandr <you@example.com>
2025-04-01 16:13:21 +0300

    add Error

--- src/components/FormTodo.tsx
+++ src/components/FormTodo.tsx
@@ -4,6 +4,7 @@ import { FormTodoProps } from "../assets/models/todo"
 
 const FormTodo: React.FC<FormTodoProps> = ({ addTodo }) => {
   const [text, setText] = useState("")
+  const [error, setError] = useState<string | null>(null)
 
   const inputRef = useRef<HTMLInputElement>(null)
 
@@ -13,17 +14,18 @@ const FormTodo: React.FC<FormTodoProps> = ({ addTodo }) => {
     const enteredText = text.trim()
 
     if (enteredText.length <= 2) {
-      console.error("Text should be longer than 2 characters")
+      setError("Text should be longer than 2 characters")
       inputRef.current?.focus()
       return
     }
     if (enteredText.length > 64) {
-      console.error("The text must be shorter than 64 characters.")
+      setError("The text must be shorter than 64 characters.")
       inputRef.current?.focus()
       return
     }
     addTodo(enteredText)
     setText("")
+    setError(null)
     inputRef.current?.focus()
   }
 
@@ -32,13 +34,15 @@ const FormTodo: React.FC<FormTodoProps> = ({ addTodo }) => {
       <input
         placeholder="Task To Be Done"
         required
-        minLength={3}
-        maxLength={64}
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={(e) => {
+          setText(e.target.value)
+          if (error) setError(null)
+        }}
         ref={inputRef}
       />
       <button type="submit">Add</button>
+      {error && <p style={{ color: "red" }}>{error}</p>}
     </form>
   )
 }
